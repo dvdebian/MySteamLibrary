3fbb5e8196c2564f506b7ae42dfc847e4ac5a7c7
dvdinux <dvdinux@gmail.com>
2025-12-30 16:07:18 +0100

    Sarted updating structure, Steamgame class moved to Models, SteamApiService created in Services but still empty

--- MySteamLibrary/MainWindow.xaml.cs
+++ MySteamLibrary/MainWindow.xaml.cs
@@ -15,58 +15,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using MySteamLibrary.Models;
 
 namespace MySteamLibrary
 {
-    // Class representing a single Steam game and its properties
-    public class SteamGame : INotifyPropertyChanged
-    {
-        // --- Variables / Properties ---
-
-        // The name of the game
-        public string Name { get; set; }
-
-        // The unique Steam Application ID
-        public int AppId { get; set; }
-
-        // Formatted string showing how long the game has been played
-        public string Playtime { get; set; }
-
-        // URL for the small square icon
-        public string IconUrl { get; set; }
-
-        // URL for the large library artwork
-        public string ImageUrl { get; set; }
-
-        // Backing field for the game description
-        private string _description;
-
-        // The textual description of the game with change notification
-        public string Description
-        {
-            get => _description;
-            set { _description = value; OnPropertyChanged(nameof(Description)); }
-        }
-
-        // Backing field for the actual image displayed in the UI
-        private object _displayImage;
-
-        // The image object (Bitmap or URL) used by the UI; ignored during JSON serialization
-        [JsonIgnore]
-        public object DisplayImage
-        {
-            get => _displayImage;
-            set { _displayImage = value; OnPropertyChanged(nameof(DisplayImage)); }
-        }
-
-        // --- Notification Logic ---
-
-        // Event triggered when a property value changes to update the UI
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // Helper method to raise the PropertyChanged event
-        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
+    
 
     // Main logic for the application window
     public partial class MainWindow : Window
---
+++ MySteamLibrary/Models/SteamClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MySteamLibrary.Models
+{
+    // Class representing a single Steam game and its properties
+    public class SteamGame : INotifyPropertyChanged
+    {
+        // --- Variables / Properties ---
+
+        // The name of the game
+        public string Name { get; set; }
+
+        // The unique Steam Application ID
+        public int AppId { get; set; }
+
+        // Formatted string showing how long the game has been played
+        public string Playtime { get; set; }
+
+        // URL for the small square icon
+        public string IconUrl { get; set; }
+
+        // URL for the large library artwork
+        public string ImageUrl { get; set; }
+
+        // Backing field for the game description
+        private string _description;
+
+        // The textual description of the game with change notification
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged(nameof(Description)); }
+        }
+
+        // Backing field for the actual image displayed in the UI
+        private object _displayImage;
+
+        // The image object (Bitmap or URL) used by the UI; ignored during JSON serialization
+        [JsonIgnore]
+        public object DisplayImage
+        {
+            get => _displayImage;
+            set { _displayImage = value; OnPropertyChanged(nameof(DisplayImage)); }
+        }
+
+        // --- Notification Logic ---
+
+        // Event triggered when a property value changes to update the UI
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Helper method to raise the PropertyChanged event
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
